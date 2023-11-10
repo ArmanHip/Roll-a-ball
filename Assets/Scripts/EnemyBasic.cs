a656606fc3ca6598a0b0252766e90b256e30f274
Arman <144450534+JoeMac12@users.noreply.github.com>
2023-11-09 22:39:45 -0400

    Make Enemy movement smoother

--- Assets/Scripts/EnemyBasic.cs
+++ Assets/Scripts/EnemyBasic.cs
@@ -6,8 +6,11 @@ public class EnemyBasic : MonoBehaviour
 {
     public Transform player;
     public float moveSpeed = 5f;
+    public float rotationSpeed = 5f;
+    public float smoothTime = 1F;
 
     private Rigidbody rb;
+    private Vector3 velocity = Vector3.zero;
 
     private void Start()
     {
@@ -16,7 +19,18 @@ public class EnemyBasic : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 direction = (player.position - transform.position).normalized;
-        rb.MovePosition(transform.position + direction * moveSpeed * Time.fixedDeltaTime);
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0; 
+
+        if (direction.magnitude > 0.1f) 
+        {
+            direction.Normalize();
+
+            Vector3 targetPosition = transform.position + direction * moveSpeed;
+            rb.MovePosition(Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime));
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
