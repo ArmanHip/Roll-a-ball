05b6fbd5f01f8ce2c0c4bea3d75fa92653a33edd
Arman <josephmacdonald4629@gmail.com>
2023-11-24 11:45:27 -0400

    Make a simple explosion effect and add it to script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    public GameObject explosionEffect; 
 
     public void TakeDamage(float amount)
     {
@@ -17,6 +18,23 @@ public class Target : MonoBehaviour
 
     void Die()
     {
+        if (explosionEffect != null)
+        {
+            GameObject instantiatedExplosionEffect = Instantiate(explosionEffect, transform.position, transform.rotation);
+
+            ParticleSystem ps = instantiatedExplosionEffect.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                float totalDuration = ps.main.duration + ps.main.startLifetime.constantMax;
+
+                Destroy(instantiatedExplosionEffect, totalDuration);
+            }
+            else
+            {
+                Destroy(instantiatedExplosionEffect, 5f);
+            }
+        }
+
         Destroy(gameObject);
     }
 }
