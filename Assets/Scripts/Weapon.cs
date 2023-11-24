33b23ecde3062eec5097ea1c7eb917ae9cdf65b2
Arman <josephmacdonald4629@gmail.com>
2023-11-24 11:18:10 -0400

    Make the weapon automatic and fix

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,27 +9,32 @@ public class Weapon : MonoBehaviour
     public Camera fpsCam;
     public GameObject muzzleFlash;
 
-    public Transform gunBarrell;
+    public Transform gunBarrel;
     public TrailRenderer bulletTrail;
+
+    private bool isShooting;
+    public float fireRate = 0.25f; 
+    private float fireTime = 0f;
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && !PauseMenu.Paused && !PlayerHealth.IsPlayerDead)
+        if (Input.GetButton("Fire1") && !PauseMenu.Paused && !PlayerHealth.IsPlayerDead && !CountdownTimer.WinPanelActive && Time.time >= fireTime)
         {
+            fireTime = Time.time + fireRate;
             Shoot();
         }
     }
 
     void Shoot()
     {
-        var bullet = Instantiate(bulletTrail, gunBarrell.position, Quaternion.identity);
-        bullet.AddPosition(gunBarrell.position);
+        var bullet = Instantiate(bulletTrail, gunBarrel.position, Quaternion.identity);
+        bullet.AddPosition(gunBarrel.position);
         {
             bullet.transform.position = transform.position + (fpsCam.transform.forward * 200);
         }
 
-       muzzleFlash.SetActive(true);
-       Invoke(nameof(DisableMuzzleFlash), 0.1f);
+        muzzleFlash.SetActive(true);
+        Invoke(nameof(DisableMuzzleFlash), 0.1f);
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
