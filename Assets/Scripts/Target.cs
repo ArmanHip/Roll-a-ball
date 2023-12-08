c77743fb2f508af029fcd41f92820d170d2332b2
Arman <144450534+JoeMac12@users.noreply.github.com>
2023-12-08 01:46:59 -0400

    new explosion sound for enemies

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
-    public GameObject explosionEffect; 
+    public GameObject explosionEffect;
+    public GameObject soundPlayerPrefab;
 
     public void TakeDamage(float amount)
     {
@@ -20,21 +21,39 @@ public class Target : MonoBehaviour
     {
         if (explosionEffect != null)
         {
-            GameObject instantiatedExplosionEffect = Instantiate(explosionEffect, transform.position, transform.rotation);
+            GameObject instantiatedExplosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+            ParticleSystem explosionParticles = instantiatedExplosion.GetComponent<ParticleSystem>();
 
-            ParticleSystem ps = instantiatedExplosionEffect.GetComponent<ParticleSystem>();
-            if (ps != null)
+            if (explosionParticles != null)
             {
-                float totalDuration = ps.main.duration + ps.main.startLifetime.constantMax;
-
-                Destroy(instantiatedExplosionEffect, totalDuration);
+                float duration = explosionParticles.main.duration + explosionParticles.main.startLifetime.constantMax;
+                Destroy(instantiatedExplosion, duration);
             }
             else
             {
-                Destroy(instantiatedExplosionEffect, 5f);
+                Destroy(instantiatedExplosion, 1f);
             }
         }
 
+        PlayDestructionSound();
+
         Destroy(gameObject);
+    }
+
+    void PlayDestructionSound()
+    {
+        if (soundPlayerPrefab != null)
+        {
+            GameObject soundPlayer = Instantiate(soundPlayerPrefab, transform.position, Quaternion.identity);
+            AudioSource audioSource = soundPlayer.GetComponent<AudioSource>();
+
+            if (audioSource != null)
+            {
+                audioSource.pitch = Random.Range(0.8f, 1.2f);
+                audioSource.Play();
+
+                Destroy(soundPlayer, audioSource.clip.length);
+            }
+        }
     }
 }
